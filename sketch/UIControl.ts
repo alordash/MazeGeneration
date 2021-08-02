fd1721af7d4ff7895ca2ec4af7395d3ad1c8480e
alordash <alordash@mail.ru>
2021-08-02 14:29:40 +0300

    Binded input values to CanvasManager's values

--- sketch/UIControl.ts
+++ sketch/UIControl.ts
@@ -1,4 +1,9 @@
 document.getElementById("WidthInput").onchange = (ev) => {
-    
+    canvasManager.width = parseInt((<HTMLInputElement>document.getElementById("WidthInput")).value);
 };
+
+document.getElementById("HeightInput").onchange = (ev) => {
+    canvasManager.height = parseInt((<HTMLInputElement>document.getElementById("HeightInput")).value);
+};
+
 (<HTMLInputElement>document.getElementById("HeightInput"));
